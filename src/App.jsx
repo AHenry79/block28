5d01d24b6d9cd115d03c2739305f41e5e7a940c8
AHenry79 <austinhenry7799@gmail.com>
2024-05-14 12:46:39 -0400

    Add styling and hamburger menu with working links

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Blue from "./components/Blue";
 import Red from "./components/Red";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Purple from "./components/Purple";
+import Green from "./components/Green";
+import Orange from "./components/Orange";
+import Pink from "./components/Pink";
 
 function App() {
   return (
     <>
-      <div id="container">
-        <div id="navbar">
-          <Link to={"/blue"}>Blue</Link>
-          <Link to={"/red"}>Red</Link>
-          <Link to={"/"}>Home</Link>
-        </div>
-        <div id="main-section">
-          <Routes>
-            <Route path="/blue" element={Blue()}>
-              Blue
-            </Route>
-            <Route path="/red" element={Red()}>
-              Red
-            </Route>
-            <Route path="/" element={Home()}>
-              Home
-            </Route>
-          </Routes>
-        </div>
-      </div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/home" element={Home()}></Route>
+        <Route path="/blue" element={Blue()}></Route>
+        <Route path="/red" element={Red()}></Route>
+        <Route path="/green" element={Green()}></Route>
+        <Route path="/orange" element={Orange()}></Route>
+        <Route path="/purple" element={Purple()}></Route>
+        <Route path="/pink" element={Pink()}></Route>
+      </Routes>
     </>
   );
 }
--- src/components/Blue.jsx
+++ src/components/Blue.jsx
@@ -1,6 +1,6 @@
 export default function Blue() {
   return (
-    <div className="blue">
+    <div className={"blue"}>
       <h1>BLUE</h1>
     </div>
   );
